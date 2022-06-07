e49147e871dd7f6917e6ae8396e15c3cdb982ac3
GUMBO <daehee_kim2@tmax.co.kr>
2022-06-07 23:17:37 +0900

    Jenkins 테스트 - 빌드 에러때문에 주석처리..

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import PersonInfoSection from "./component/PersonalInfo/PersonInfoSection";
 
-
-const SectionHeightArr: number[] = [2, 3, 2, 2]
-
-const CalculateCurrentSection = (currentY: number, windowHeight: number) => {
-    let currentSection = 1;
-    let beforeSectionBottomY = 0;
-    for (const sectionHeight of SectionHeightArr) {
-        if (beforeSectionBottomY < currentY && currentY <= beforeSectionBottomY + sectionHeight * windowHeight) break;
-        beforeSectionBottomY += sectionHeight * windowHeight;
-        currentSection++;
-    }
-    return currentSection;
-}
-
-export interface SectionProps {
-    currentScrollY: number;
-    currentSection: number;
-}
+//
+// const SectionHeightArr: number[] = [2, 3, 2, 2]
+//
+// const CalculateCurrentSection = (currentY: number, windowHeight: number) => {
+//     let currentSection = 1;
+//     let beforeSectionBottomY = 0;
+//     for (const sectionHeight of SectionHeightArr) {
+//         if (beforeSectionBottomY < currentY && currentY <= beforeSectionBottomY + sectionHeight * windowHeight) break;
+//         beforeSectionBottomY += sectionHeight * windowHeight;
+//         currentSection++;
+//     }
+//     return currentSection;
+// }
+//
+// export interface SectionProps {
+//     currentScrollY: number;
+//     currentSection: number;
+// }
 
 const App: React.FC = () => {
-    const [currentScrollY, setCurrentScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
-    const [currentSection, setCurrentSection] = useState(1);
-
-    useEffect(() => {
-        window.addEventListener('scroll', () => setCurrentScrollY(window.scrollY));
-    }, [])
-
-    useEffect(() => {
-        console.log("ScrollY is ", currentScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
-        const windowHeight = window.innerHeight;
-        console.log(CalculateCurrentSection(currentScrollY, windowHeight));
-        setCurrentSection(CalculateCurrentSection(currentScrollY, windowHeight));
-    }, [currentScrollY])
+    // const [currentScrollY, setCurrentScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
+    // const [currentSection, setCurrentSection] = useState(1);
+    //
+    // useEffect(() => {
+    //     window.addEventListener('scroll', () => setCurrentScrollY(window.scrollY));
+    // }, [])
+    //
+    // useEffect(() => {
+    //     console.log("ScrollY is ", currentScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
+    //     const windowHeight = window.innerHeight;
+    //     console.log(CalculateCurrentSection(currentScrollY, windowHeight));
+    //     setCurrentSection(CalculateCurrentSection(currentScrollY, windowHeight));
+    // }, [currentScrollY])
 
     return (
         <>
