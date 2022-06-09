507b17071ddf5212b0e541b5c7c74c2ff3b8ec73
GUMBO <daehee_kim2@tmax.co.kr>
2022-06-09 12:44:23 +0900

    안쓰는 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,49 +4,13 @@ import CareerSection from "./component/2.career/CareerSection";
 import TechStackSection from "./component/3.techStack/TechStackSection";
 import ProjectSection from "./component/4.project/ProjectSection";
 
-//
-// const SectionHeightArr: number[] = [2, 3, 2, 2]
-//
-// const CalculateCurrentSection = (currentY: number, windowHeight: number) => {
-//     let currentSection = 1;
-//     let beforeSectionBottomY = 0;
-//     for (const sectionHeight of SectionHeightArr) {
-//         if (beforeSectionBottomY < currentY && currentY <= beforeSectionBottomY + sectionHeight * windowHeight) break;
-//         beforeSectionBottomY += sectionHeight * windowHeight;
-//         currentSection++;
-//     }
-//     return currentSection;
-// }
-//
-// export interface SectionProps {
-//     currentScrollY: number;
-//     currentSection: number;
-// }
-
 const App: React.FC = () => {
-    // const [currentScrollY, setCurrentScrollY] = useState(0);  // 스크롤값을 저장하기 위한 상태
-    // const [currentSection, setCurrentSection] = useState(1);
-    //
-    // useEffect(() => {
-    //     window.addEventListener('scroll', () => setCurrentScrollY(window.scrollY));
-    // }, [])
-    //
-    // useEffect(() => {
-    //     console.log("ScrollY is ", currentScrollY); // ScrollY가 변화할때마다 값을 콘솔에 출력
-    //     const windowHeight = window.innerHeight;
-    //     console.log(CalculateCurrentSection(currentScrollY, windowHeight));
-    //     setCurrentSection(CalculateCurrentSection(currentScrollY, windowHeight));
-    // }, [currentScrollY])
-
     return (
         <>
             <PersonInfoSection/>
             <CareerSection />
             <TechStackSection />
             <ProjectSection />
-            {/*<CareerSection/>*/}
-            {/*<Section3/>*/}
-            {/*<Section4/>*/}
         </>
     );
 }
