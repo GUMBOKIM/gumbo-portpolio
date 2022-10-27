3dac1fe2d20d73fcb1d1462353c0481cf7e98041
daeheekim <dae4805@naver.com>
2022-10-27 18:04:09 +0900

    수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -7,11 +7,13 @@ const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: "DungGeunMo";
+    font-display: block;
     src: url('./font/DungGeunMo.woff');
   }
 
   @font-face {
     font-family: "PressStart2P";
+    font-display: block;
     src: url('./font/PressStart2P-vaV7.woff');
   }
 `;
