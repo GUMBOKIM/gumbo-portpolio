ec53ad0745b5f5d746aed457c3a71370831c60ec
daeheekim <dae4805@naver.com>
2022-10-27 18:03:49 +0900

    로딩 화면

---
+++ src/scene/loadingScene/LoadingScene.tsx
@@ -0,0 +1,11 @@
+import * as S from "./LoadingScene.style";
+
+const LoadingScene = () => {
+  return (
+    <S.LoadingSceneContainer>
+      <h1>Loading...</h1>
+    </S.LoadingSceneContainer>
+  );
+};
+
+export default LoadingScene;
