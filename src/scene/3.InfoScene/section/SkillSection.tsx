f7c7a64f35ab17d0ae70a4e43511da74c19f9fe8
daeheekim <dae4805@naver.com>
2022-11-02 02:17:37 +0900

    Contact 제외 완성

--- src/scene/3.InfoScene/section/SkillSection.tsx
+++ src/scene/3.InfoScene/section/SkillSection.tsx
@@ -6,18 +6,15 @@ const SkillSection = () => {
     return (
         <SectionLayout>
             <h3>기술 스택</h3>
+            <S.SkillTitle>언어</S.SkillTitle>
             <S.SkillHR/>
-            <S.SkillTitle>
-                언어
-            </S.SkillTitle>
             <S.SkillItemContainer>
                 <S.SkillItem name='Java Script' src='language/javascript.png'/>
                 <S.SkillItem name='Type Script' src='language/typescript.png'/>
                 <S.SkillItem name='Java' src='language/java.png'/>
             </S.SkillItemContainer>
-            <S.SkillTitle>
-                백엔드
-            </S.SkillTitle>
+            <S.SkillTitle>백엔드</S.SkillTitle>
+            <S.SkillHR/>
             <S.SkillItemContainer>
                 <S.SkillItem name='Spring Boot' src='backend/springboot.png'/>
                 <S.SkillItem name='Jpa' src='backend/jpa.png'/>
@@ -26,9 +23,8 @@ const SkillSection = () => {
                 <S.SkillItem name='MSSQL' src='backend/mssql.png'/>
                 <S.SkillItem name='Jenkins' src='backend/jenkins.png'/>
             </S.SkillItemContainer>
-            <S.SkillTitle>
-                프론트엔드
-            </S.SkillTitle>
+            <S.SkillTitle>프론트엔드</S.SkillTitle>
+            <S.SkillHR/>
             <S.SkillItemContainer>
                 <S.SkillItem name='React' src='frontend/react.png'/>
                 <S.SkillItem name='MobX' src='frontend/mobx.png'/>
